3983013bec3c0091f9d29bd8403cdca498404f15
luwei <luwei@qiangyun.com>
2021-08-16 15:19:18 +0800

    init

--- sql/collection.sql
+++ sql/collection.sql
@@ -1,20 +1,27 @@
-DROP TABLE IF EXISTS `code_column_config`;
+DROP TABLE IF EXISTS `cases`;
 CREATE TABLE `cases` (
-      `case_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-      `case_code` varchar(255) DEFAULT NULL comment '案件编号',
-      `consignor_id` varchar(255) DEFAULT NULL comment '委托方ID',
-      `loan_time` datetime DEFAULT NULL COMMENT '放款时间',
-      `dict_name` varchar(255) DEFAULT NULL,
-      `extra` varchar(255) DEFAULT NULL,
-      `form_show` bit(1) DEFAULT NULL,
-      `form_type` varchar(255) DEFAULT NULL,
-      `key_type` varchar(255) DEFAULT NULL,
-      `list_show` bit(1) DEFAULT NULL,
-      `not_null` bit(1) DEFAULT NULL,
-      `query_type` varchar(255) DEFAULT NULL,
-      `remark` varchar(255) DEFAULT NULL,
-      `date_annotation` varchar(255) DEFAULT NULL,
-      PRIMARY KEY (`column_id`) USING BTREE,
-      KEY `idx_table_name` (`table_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='代码生成字段信息存储';
+    `case_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '案件ID',
+    `consignor_id` bigint(20) NOT NULL comment '委托方ID',
+    `loan_time` datetime DEFAULT NULL COMMENT '放款时间',
+    `loan_id` varchar(64) NOT NULL comment '放款订单号',
+    `loan_amount` decimal(10,2) NOT NULL comment '放款金额',
+    `loan_periods` tinyint(4) NOT NULL comment '放款期数'
+    PRIMARY KEY (`case_id`) USING BTREE,
+    KEY `idx_loan_id` (`loan_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='案件信息';
+
+DROP TABLE IF EXISTS `case_ditribution`;
+CREATE TABLE `cases` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `case_id` bigint(20) NOT NULL COMMENT '案件ID',
+    `dept_id` bigint(20) NOT NULL comment '部门ID',
+    `user_id` bigint(20) NOT NULL COMMENT '用户ID',
+    `loan_id` varchar(64) NOT NULL comment '放款订单号',
+    `loan_amount` decimal(10,2) NOT NULL comment '放款金额',
+    `loan_periods` tinyint(4) NOT NULL comment '放款期数'
+     PRIMARY KEY (`case_id`) USING BTREE,
+     KEY `idx_loan_id` (`loan_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='案件信息';
+
+
 
